f7e56d9c1ba38a8ff34765b5d4612a90f9ec9831
Dafdaf <81273171+Dffarhn@users.noreply.github.com>
2025-12-26 11:10:49 +0800

    Add whitespace for code readability in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  
 
   useEffect(() => {
     const handleMouseMove = (e) => {
